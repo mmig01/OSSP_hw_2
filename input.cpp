e5c5f18af52547f367d8d6a747160813d3a3187d
mmig01 <mintim01@naver.com>
2024-03-24 23:24:39 +0900

    fix: input 파일 수정

--- input.cpp
+++ input.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main() {
   string name;
   int student_num;
+  string team_name;
   cout << "이름을 입력하세요 : " << '\n';
   cin >> name;
   cout << "학번을 입력하세요 : " << '\n';
   cin >> student_num;
+  cout << "팀 이름을 입력하세요 : " << '\n';
+  cin >> team_name;
 
   cout << "<출력>\n";
   cout << "이름 : " << name << '\n';
   cout << "학번 : " << student_num << '\n';
+  cout << "팀 이름 : " << team_name << '\n';
   return 0;
 }
